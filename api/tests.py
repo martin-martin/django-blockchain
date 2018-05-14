619e672d98ff545913952d3652e5131395c95afb
martin-martin <breuss.martin@gmail.com>
2018-05-14 17:36:14 +0800

    set up db model tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Blockchain
 
-# Create your tests here.
+
+class ModelTestCase():
+    """Defines the test suite for the Blockchain model."""
+
+    def set_up(self):
+        """Defines the test client and other test variables."""
+
+        # TODO: change below to accept a new block input to database
+        self.blockchain_block = None  # change this
+        self.blockchain = Blockchain()  # add the required args
+
+    def test_model_can_create_a_block(self):
+        """Test if the model can create a block."""
+
+        old_count = Blockchain.objects.count()
+        self.blockchain.save()
+        new_count = Blockchain.objects.count()
+        self.assertNotEqual(old_count, new_count)
